dab246600b1a28ba83283ad1311d9bf357fed49c
umemuraKyoji <153409294+umemuraKyoji@users.noreply.github.com>
2023-12-17 14:26:24 +0900

    Message-20231219

--- s4/umemura/Hello.java
+++ s4/umemura/Hello.java
@@ -3,6 +3,6 @@ import java.lang.*;
 
 public class Hello {
     public static void main(String[] args) {
-	System.out.println("hello..(umemura/Update)");
+	System.out.println("hello..(umemura/2nd Update)");
     }
 }
